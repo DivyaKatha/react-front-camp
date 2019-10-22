734afd591e39f5792b48f56189bbb1b78df6b947
Sridivya Katha <Sridivya_Katha@epam.com>
2019-10-22 16:11:25 +0530

    Added no movie found component

---
+++ movie-app/src/NoMovieFound/NoMovieFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NoMovieFound.css';
+
+const NoMovieFound = () => {
+    return <div className="noMovie">
+        <h1>No movies found</h1>
+    </div>
+}
+
+export default NoMovieFound;
--- movie-app/src/Results/Results.js
+++ movie-app/src/Results/Results.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 import Movie from '../Movie/Movie';
 import './Results.css'
+import NoMovieFound from '../NoMovieFound/NoMovieFound';
 
 class Results extends Component {
    
     render() {
-        return (
+        if(this.props.movies.length) return (
         <div className="results" id="results">{
             this.props.movies.map((movie, index) => {
                 return <Movie movie={movie} key={movie.id} selected={this.props.selectMovie}></Movie>
             })
         }</div>
-        );
+        ); else return (<NoMovieFound></NoMovieFound>)
     }
 }
 
